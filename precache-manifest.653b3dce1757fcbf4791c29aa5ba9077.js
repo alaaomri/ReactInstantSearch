940a0d5dc70f14a43c9bbfb66d68718cf734bb94
alaaomri <omrialaaeddine@gmail.com>
2019-03-11 09:54:44 +0100

    Updates

---
+++ precache-manifest.653b3dce1757fcbf4791c29aa5ba9077.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "846329e494e581d7fd09",
+    "url": "/static/js/main.846329e4.chunk.js"
+  },
+  {
+    "revision": "e8688bcb0871dd773c03",
+    "url": "/static/js/2.e8688bcb.chunk.js"
+  },
+  {
+    "revision": "846329e494e581d7fd09",
+    "url": "/static/css/main.68e556c7.chunk.css"
+  },
+  {
+    "revision": "064a4602978fe9040652fb6a25dfa1c0",
+    "url": "/index.html"
+  }
+];

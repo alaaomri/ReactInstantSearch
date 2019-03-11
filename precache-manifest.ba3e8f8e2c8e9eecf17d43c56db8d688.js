bbc459a07205e73e42ed7fd35f11bf1bd5f6ecf3
alaaomri <omrialaaeddine@gmail.com>
2019-03-11 11:02:09 +0100

    Updates

---
+++ precache-manifest.ba3e8f8e2c8e9eecf17d43c56db8d688.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc7ebbbfc1e3716a3b7b",
+    "url": "/ReactInstantSearch/static/js/runtime~main.dc7ebbbf.js"
+  },
+  {
+    "revision": "d0123cea644fcbaa0ba1",
+    "url": "/ReactInstantSearch/static/js/main.d0123cea.chunk.js"
+  },
+  {
+    "revision": "aaf6caaa258039b5eae8",
+    "url": "/ReactInstantSearch/static/js/2.aaf6caaa.chunk.js"
+  },
+  {
+    "revision": "d0123cea644fcbaa0ba1",
+    "url": "/ReactInstantSearch/static/css/main.68e556c7.chunk.css"
+  },
+  {
+    "revision": "6c1b978dd238d58575c7cea6d440898d",
+    "url": "/ReactInstantSearch/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.653b3dce1757fcbf4791c29aa5ba9077.js"
+  "/ReactInstantSearch/precache-manifest.ba3e8f8e2c8e9eecf17d43c56db8d688.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactInstantSearch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
